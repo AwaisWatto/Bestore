17f9a662c8f28228b497cc4db62a0ac069ee9b04
AwaisWatto <116358085+AwaisWatto@users.noreply.github.com>
2022-11-03 21:34:27 +0500

    Add files via upload

---
+++ Parent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar1 from "./Navbar1";
+import Navbar2 from "./Navbar2";
+import Navbar3 from "./Navbar3";
+
+function Example()
+{
+
+return(   
+<div>
+<Navbar1 />
+<Navbar2 />
+<Navbar3 />
+</div>
+
+
+);
+}
+
+export default Example;
